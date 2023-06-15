ca4ad15d98b142cc54fecc22b5d4a27d59e2d7d2
egekulahli <egekulahli@gmail.com>
2023-06-16 02:33:06 +0300

    initial changes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const server = require('./api/server');
-
+console.log('hello world');
 const port = 9000;
 
+server.listen(port, () => {
+    console.log("Server is listening on " + port);
+});
 // START YOUR SERVER HERE
